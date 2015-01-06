d8eccf6fbaeef45836ac79383d477b2639acc641
hotoo <hotoo.cn@gmail.com>
2015-01-06 16:12:26 +0800

    Add timing and example

--- examples/index.md
+++ examples/index.md
@@ -2,10 +2,55 @@
 
 ---
 
-## Normal usage
+<script type='text/javascript' src='http://s3u.github.io/har-view/scripts/jquery.min.js'></script>
+<script type='text/javascript' src='http://s3u.github.io/har-view/scripts/jquery-ui.min.js'></script>
+<script type='text/javascript' src='http://s3u.github.io/har-view/scripts/humanize.min.js'></script>
+<script type='text/javascript' src='http://s3u.github.io/har-view/scripts/har-viewer.js'></script>
+<script type='text/javascript' src='http://s3u.github.io/har-view/scripts/mustache.js'></script>
+<link rel='stylesheet' href='http://s3u.github.io/har-view/css/jquery-ui.css'/>
+<link rel='stylesheet' href='http://s3u.github.io/har-view/css/har-viewer.css'/>
+
+<script src="https://a.alipayobjects.com/seajs/seajs/2.2.3/sea.js"></script>
+
+<div id="har-view"></div>
+
+<pre id="perf"></pre>
 
 ````javascript
-seajs.use('index', function(saiPerf) {
+seajs.use(['index', 'jquery'], function(Performance, $) {
+
+  function repeat(string, times){
+    return new Array(times + 1).join(string);
+  }
+
+  $(window).on("load", function(){
+    window.setTimeout(function(){
+      var har_data = Performance.perf();
+      console.log("Perf", har_data);
+
+
+      jQuery('#har-view').HarView();
+      var har_viewer = jQuery('#har-view').data('HarView');
+      har_viewer.render(har_data);
+
+      var indent = 0;
+      $("#perf").html(JSON.stringify(har_data).replace(/\{|\},|\}|["0-9],/g, function($0){
+        if ($0 === '{') {
+          indent += 2;
+        } else if ( $0 === '},') {
+          indent -= 2;
+          $0 = '<br />' + repeat(' ', indent) + $0;
+        } else if ( $0 === '}') {
+          indent -= 2;
+          $0 = '<br />' + repeat(' ', indent) + $0;
+        } else if ($0 === '",') {
+        }
+        return $0 + '<br/>' + repeat(" ", indent);
+      }));
+    }, 200)
+  });
 
 });
 ````
+
+<iframe width="1" height="1" src="http://www.baidu.com"></iframe>
--- index.js
+++ index.js
@@ -1,3 +1,160 @@
-var saiPerf;
 
-module.exports = saiPerf;
+var win = window;
+var doc = win.document;
+var loc = win.location;
+var isFrame = win !== top;
+
+var Sai = require("sai");
+var ready = require("ready");
+var detector = require("detector");
+
+var HAR = {
+  "log": {
+    "version" : "1.2",
+    "creator" : {
+      "name": "Sai-perf",
+      "version": "<%=version%>"
+    },
+    "browser" : {
+      "name": detector.browser.name,
+      "version": detector.browser.fullVersion
+    },
+    "pages": [],
+    "entries": []
+  }
+};
+
+var Performance = {
+  wait: 0,
+  redirect: 0,
+  appcache: 0,
+  unload: 0,
+  dnslookup: 0,
+  tcpconnect: 0,
+  requestTime: 0,
+  initDomTreeTime: 0,
+  domReadyTime: 0,
+  loadEventTime: 0,
+  loadTime: 0
+};
+
+function getPerformance(timing, navigationStart){
+  return {
+    wait : timing.fetchStart - navigationStart,
+    redirect: timing.redirectEnd  - timing.redirectStart,
+    appCache: timing.domainLookupStart  - timing.fetchStart,
+    dnslookup: timing.domainLookupEnd - timing.domainLookupStart,
+    tcpconnect: timing.connectEnd - timing.connectStart,
+    request: timing.responseStart - timing.requestStart,
+    response: timing.responseEnd - timing.responseStart,
+
+    TTFB: timing.responseStart - navigationStart,
+  };
+}
+
+function merge(origin, target) {
+  for(var key in target){
+    if (target.hasOwnProperty(key)) {
+      origin[key] = target[key];
+    }
+  }
+  return origin;
+}
+
+function sum(timing, startTime){
+  return timing.responseEnd - startTime;
+
+  var total = 0;
+  for(var key in timing){
+    if (timing.hasOwnProperty(key) && timing[key] !== -1) {
+      total += timing[key];
+    }
+  }
+  return total;
+}
+
+Sai.perf = function(){
+  var performance = {};
+  var performanceTiming = win.performance.timing;
+  var navigationStart = performanceTiming.navigationStart;
+
+  var page_id = "p0";
+
+  HAR.log.pages.push({
+    "startedDateTime": 0,
+    "id": page_id,
+    "pageTimings": {
+      "onContentLoad": performanceTiming.domContentLoadedEventEnd - navigationStart,
+      "onLoad": performanceTiming.loadEventStart === 0 ? 0 : performanceTiming.loadEventStart - navigationStart, // loadEventStart:0
+      "_TTI": performanceTiming.domInteractive - navigationStart // CUSTOM FIELD.
+      //FIRST_PAINT: "-",
+    }
+  });
+
+  HAR.log.entries.push({
+    "pageref": page_id,
+    "startedDateTime": 0,
+    "time": performanceTiming.responseEnd - navigationStart,
+    "request": {
+      "method": "",
+      "url": loc.href.split("#")[0]
+    },
+    "timings": {
+      "blocked": performanceTiming.fetchStart - navigationStart, // stalled.
+      "dns": performanceTiming.domainLookupEnd - performanceTiming.domainLookupStart, // dnslookup.
+      "connect": performanceTiming.connectEnd - performanceTiming.connectStart, // tcp connect.
+      "send": performanceTiming.responseStart - performanceTiming.requestStart, // request.
+      "receive": performanceTiming.responseEnd - performanceTiming.responseStart, // response
+      "ssl": performanceTiming.secureConnectionStart === 0 ? -1 : performanceTiming.secureConnectionStart - performanceTiming.connectStart
+    }
+  });
+
+  //performance[loc.href] = merge(getPerformance(performanceTiming, navigationStart),
+    //{
+      //unload: performanceTiming.unloadEventEnd - performanceTiming.unloadEventStart, // 0, referrer page unload event.
+      //interactive: performanceTiming.domInteractive - performanceTiming.domLoading,
+      //domready: performanceTiming.domContentLoadedEventStart - performanceTiming.domInteractive, // 过早获取时 domComplete 有时会是 0
+      //domcomplete: performanceTiming.domComplete === 0 ? 0 : performanceTiming.domComplete - performanceTiming.domLoading, // domComplete:0
+      //load: performanceTiming.loadEventEnd - performanceTiming.loadEventStart, // 0
+    //}
+  //);
+
+  if (!win.performance.getEntriesByType) {
+    return HAR;
+  }
+
+  var resourceList = win.performance.getEntriesByType("resource");
+
+  for (var i = 0, l = resourceList.length, resourceTiming; i < l; i++) {
+    resourceTiming = resourceList[i];
+    //performance[resourceTiming.name] = getPerformance(resourceTiming, navigationStart);
+
+    HAR.log.entries.push({
+      "pageref": page_id,
+      "startedDateTime": resourceTiming.startTime,
+      "time": resourceTiming.duration,
+      "request": {
+        "method": "GET",
+        "url": resourceTiming.name
+      },
+      "timings": {
+        "blocked": resourceTiming.fetchStart - resourceTiming.startTime, // stalled.
+        "dns": resourceTiming.domainLookupEnd - resourceTiming.domainLookupStart, // dnslookup.
+        "connect": resourceTiming.connectEnd - resourceTiming.connectStart, // tcp connect.
+        "send": resourceTiming.responseStart - resourceTiming.requestStart, // request.
+        "receive": resourceTiming.responseEnd - resourceTiming.responseStart, // response
+        "ssl": resourceTiming.secureConnectionStart === 0 ? -1 : resourceTiming.secureConnectionStart - resourceTiming.connectStart
+      }
+    });
+  }
+
+  return HAR;
+};
+
+win.onbeforeunload = function(){
+  //Sai.log({
+    //"test": "TEST"
+  //})
+};
+
+module.exports = Sai;
